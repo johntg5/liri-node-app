fedc66c9c291f278502816794631d3a84422e727
Tyler Johnson <johntg5@gmail.com>
2017-11-29 22:28:44 +0000

    first commit

---
+++ keys.js
@@ -0,0 +1,19 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'tBKWxScO99MMRTSnkqqbNQ3u3',
+    consumer_secret: 'gNLSA1cToQxSl1rifro7BQnmO8gr81JNmaNjFRUFPTXAvnsFvw',
+    access_token_key: '935589388997873665-eGeyFD85f3qAFOAOyi5PZ7cVwcdcJTD',
+    access_token_secret: 'MKVzy6s8Pdm8OEeFx68UrMBqvUECIPF1qX9Qksz6HBJGx',
+};
+
+module.exports = twitterKeys;
+
+
+
+
+
+//Spotify
+
+client_id: "ecc0d7f569e74f4fb57815a79dfc597e"
+client_secret: "9b490f365e63444990ce1bf84d7c46da"
